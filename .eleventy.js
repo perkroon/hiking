afd2098f01e319117c27279303cc8f6bb79f662f
Per Kroon <per.kroon@gmail.com>
2022-08-20 17:21:32 +0200

    Copy photos. Fix photo paths

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,8 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPassthroughCopy("**/*.png");
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
